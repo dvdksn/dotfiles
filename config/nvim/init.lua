712860a5d0304681a6499cd73b43906cac2be563
David Karlsson <davidkarlsson0@gmail.com>
2022-05-23 15:24:19 +0200

    Fixed disabling formatting capabilities for language servers

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -24,55 +24,61 @@ o.shiftwidth = 2
 o.expandtab = true
 
 require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
-	use("hoob3rt/lualine.nvim")
-	use("kyazdani42/nvim-web-devicons")
-	use("kyazdani42/nvim-tree.lua")
-	use("romgrk/barbar.nvim")
-	use({ "folke/tokyonight.nvim", branch = "main" })
-	use("nvim-lua/popup.nvim")
-	use("nvim-lua/plenary.nvim")
-	use("nvim-telescope/telescope.nvim")
-	use({
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	})
-	use("lukas-reineke/indent-blankline.nvim")
-	use("neovim/nvim-lspconfig")
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-	use("folke/trouble.nvim")
-	use({
-		"tami5/lspsaga.nvim",
-		config = function()
-			require("lspsaga").init_lsp_saga({
-				max_preview_lines = 10,
-				border_style = "round",
-				code_action_prompt = {
-					virtual_text = false,
-				},
-			})
-		end,
-	})
-	use({
-		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
-	})
-	use("windwp/nvim-ts-autotag")
-	use("yamatsum/nvim-cursorline")
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("ellisonleao/glow.nvim")
-	use("williamboman/nvim-lsp-installer")
-	use("jose-elias-alvarez/null-ls.nvim")
-	use("onsails/lspkind.nvim")
-	use("L3MON4D3/LuaSnip")
-	use("hrsh7th/cmp-path")
-	use("saadparwaiz1/cmp_luasnip")
-	use("gpanders/editorconfig.nvim")
+  use("wbthomason/packer.nvim")
+  use("hoob3rt/lualine.nvim")
+  use("kyazdani42/nvim-web-devicons")
+  use("kyazdani42/nvim-tree.lua")
+  use("romgrk/barbar.nvim")
+  use({ "folke/tokyonight.nvim", branch = "main" })
+  use("nvim-lua/popup.nvim")
+  use("nvim-lua/plenary.nvim")
+  use("nvim-telescope/telescope.nvim")
+  use({
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+  })
+  use("lukas-reineke/indent-blankline.nvim")
+  use("neovim/nvim-lspconfig")
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+  use("folke/trouble.nvim")
+  use({
+    "tami5/lspsaga.nvim",
+    config = function()
+      require("lspsaga").init_lsp_saga({
+        max_preview_lines = 10,
+        border_style = "round",
+        code_action_prompt = {
+          virtual_text = false,
+        },
+      })
+    end,
+  })
+  use({
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup({})
+    end,
+  })
+  use("windwp/nvim-ts-autotag")
+  use("yamatsum/nvim-cursorline")
+  use("hrsh7th/nvim-cmp")
+  use("hrsh7th/cmp-nvim-lsp")
+  use("ellisonleao/glow.nvim")
+  use("williamboman/nvim-lsp-installer")
+  use("jose-elias-alvarez/null-ls.nvim")
+  use("onsails/lspkind.nvim")
+  use("L3MON4D3/LuaSnip")
+  use("hrsh7th/cmp-path")
+  use("saadparwaiz1/cmp_luasnip")
+  use("gpanders/editorconfig.nvim")
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
 end)
 
 -- Color scheme:
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -6,143 +6,140 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-
-
 -- Generic on_attach
--- Do nothing, LSP mappings made generic...
 local on_attach = function()
-	local opts = { noremap = true, silent = true }
-	local map = vim.api.nvim_set_keymap
+  local opts = { noremap = true, silent = true }
+  local map = vim.api.nvim_set_keymap
 
-	map("n", "<space>f", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
-	map("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	map("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	map("n", "gp", '<Cmd>lua require"lspsaga.provider".preview_definition()<CR>', opts)
-	map("n", "K", "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", opts)
-	map("n", "<F2>", "<cmd>lua require('lspsaga.rename').rename()<CR>", opts)
-	map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	map("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	map("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	map("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-	map("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-	map("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	map("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	map("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-	map("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+  map("n", "<space>f", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+  map("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  map("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  map("n", "gp", '<Cmd>lua require"lspsaga.provider".preview_definition()<CR>', opts)
+  map("n", "K", "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", opts)
+  map("n", "<F2>", "<cmd>lua require('lspsaga.rename').rename()<CR>", opts)
+  map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  map("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  map("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+  map("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+  map("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+  map("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  map("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  map("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+  map("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
 end
 
 require("nvim-lsp-installer").setup({
-	ensure_installed = {
-		"bashls",
-		"golangci_lint",
-		"gopls",
-		"html",
-		"jsonls",
-		"cssls",
-		"pyright",
-		"sumneko_lua",
-		"svelte",
-		"tsserver",
-		"yamlls",
-	},
-	automatic_installation = true,
+  ensure_installed = {
+    "bashls",
+    "golangci_lint",
+    "gopls",
+    "html",
+    "jsonls",
+    "cssls",
+    "pyright",
+    "sumneko_lua",
+    "svelte",
+    "tsserver",
+    "yamlls",
+  },
+  automatic_installation = true,
 })
 
 local null_ls = require("null-ls")
 
 null_ls.setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	sources = {
-		null_ls.builtins.diagnostics.vale,
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.black,
-	},
+  capabilities = capabilities,
+  on_attach = on_attach,
+  sources = {
+    null_ls.builtins.diagnostics.vale,
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.black,
+  },
 })
 
 lspconfig.bashls.setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = on_attach,
 })
 lspconfig.pyright.setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = on_attach,
 })
 lspconfig.svelte.setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = on_attach,
 })
 lspconfig.tsserver.setup({
-	capabilities = capabilities,
-	on_attach = function(client)
-		client.server_capabilities.document_formatting = false
-		client.server_capabilities.document_range_formatting = false
+  capabilities = capabilities,
+  on_attach = function(client)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
 
-		on_attach()
-	end,
+    on_attach()
+  end,
 })
 lspconfig.yamlls.setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = on_attach,
 })
 
 lspconfig.gopls.setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	settings = {
-		gopls = {
-			experimentalPostfixCompletions = true,
-			analyses = {
-				unusedparams = true,
-				shadow = true,
-			},
-			staticcheck = true,
-		},
-	},
-	init_options = {
-		usePlaceholders = true,
-	},
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    gopls = {
+      experimentalPostfixCompletions = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+      staticcheck = true,
+    },
+  },
+  init_options = {
+    usePlaceholders = true,
+  },
 })
 
 lspconfig.sumneko_lua.setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-		},
-	},
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
 })
 
 lspconfig.jsonls.setup({
-	capabilities = capabilities,
-	on_attach = function(client)
-		client.server_capabilities.document_formatting = false
-		client.server_capabilities.document_range_formatting = false
+  capabilities = capabilities,
+  on_attach = function(client)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
 
-		on_attach()
-	end,
+    on_attach()
+  end,
 })
 
 lspconfig.html.setup({
-	capabilities = capabilities,
-	on_attach = function(client)
-		client.server_capabilities.document_formatting = false
-		client.server_capabilities.document_range_formatting = false
+  capabilities = capabilities,
+  on_attach = function(client)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
 
-		on_attach()
-	end,
+    on_attach()
+  end,
 })
 
 lspconfig.cssls.setup({
-	capabilities = capabilities,
-	on_attach = function(client)
-		client.server_capabilities.document_formatting = false
-		client.server_capabilities.document_range_formatting = false
+  capabilities = capabilities,
+  on_attach = function(client)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
 
-		on_attach()
-	end,
+    on_attach()
+  end,
 })
