6319d9d51adb2be36366818c17d897dad6f24f7e
David Karlsson <davidkarlsson0@gmail.com>
2022-05-24 23:07:01 +0200

    updated lir styles

--- config/nvim/lua/plugins/lir.lua
+++ config/nvim/lua/plugins/lir.lua
@@ -9,29 +9,45 @@ require 'lir'.setup {
   show_hidden_files = true,
   devicons_enable = true,
   float = {
-    winblend = 15,
+    winblend = 0,
     curdir_window = {
       enable = true,
       highlight_dirname = true
     },
+    win_opts = function()
+      local width = math.floor(vim.o.columns * 0.5)
+      local height = math.floor(vim.o.lines * 0.5)
+      return {
+        border = {
+          "•", "─", "•", "│", "•", "─", "•", "│",
+        },
+        width = width,
+        height = height,
+        style = "minimal",
+        row = math.floor((vim.o.lines - height) / 2),
+        col = math.floor((vim.o.columns - width) / 2),
+      }
+    end,
   },
   hide_cursor = true,
   mappings = {
-    ['l']     = actions.edit,
-    ['<C-s>'] = actions.split,
-    ['<C-v>'] = actions.vsplit,
-    ['<C-t>'] = actions.tabedit,
-    ['h']     = actions.up,
-    ['q']     = actions.quit,
-    ['K']     = actions.mkdir,
-    ['N']     = actions.newfile,
-    ['<F2>']  = actions.rename,
-    ['Y']     = actions.yank_path,
-    ['.']     = actions.toggle_show_hidden,
-    ['D']     = actions.delete,
+    ['l']    = actions.edit,
+    ['s']    = actions.split,
+    ['v']    = actions.vsplit,
+    ['t']    = actions.tabedit,
+    ['h']    = actions.up,
+    ['q']    = actions.quit,
+    ['K']    = actions.mkdir,
+    ['N']    = actions.newfile,
+    ['<F2>'] = actions.rename,
+    ['Y']    = actions.yank_path,
+    ['.']    = actions.toggle_show_hidden,
+    ['D']    = actions.delete,
   },
 }
 
+vim.cmd[[au VimEnter * highlight FloatBorder guibg=NONE]]
+
 local map = vim.api.nvim_set_keymap
 map("n", "<C-n>", ":lua require'lir.float'.toggle()<CR>", { noremap = true, silent = true })
-map("n", "<C-h>", ":edit %:h<CR>", { noremap = true, silent = true })
+map("n", "<leader>h", ":edit %:h<CR>", { noremap = true, silent = true })
