3f181ce0a6e4b9b3bf9682dab60d10651ade0d6a
David Karlsson <davidkarlsson0@gmail.com>
2022-06-23 07:55:36 +0200

    Add new hug doc snippet

--- config/nvim/lua/plugins/snippets.lua
+++ config/nvim/lua/plugins/snippets.lua
@@ -16,7 +16,7 @@ local lambda = require("luasnip.extras").l
 local parse = ls.parser.parse_snippet
 
 ls.config.set_config({
-	delete_check_events = "TextChanged"
+  delete_check_events = "TextChanged"
 })
 
 -- vim.keymap.set({ "i", "n" }, "<C-i>", function()
@@ -28,59 +28,80 @@ ls.config.set_config({
 require("luasnip.session.snippet_collection").clear_snippets("go")
 
 ls.add_snippets("go", {
-	-- LuaSnip syntax
-	s({
-		trig = "iferr",
-		name = "Handle error",
-		dscr = "If error does not equal nil, handle error or return",
-		docstring = "if err != nil {\n\treturn err\n}",
-	}, {
-		t({ "if err != nil {", "\t" }),
-		i(0, "return err"),
-		t({ "", "}" }),
-	}),
-	-- LSP syntax
-	parse({
-		trig = "func",
-	}, "func $1($2) $3 {\n\t$0\n}"),
-	-- "fmt" syntax
-	s({
-		trig = "fmain",
-		docstring = "func main() {\n\t\n}",
-	}, fmt("func main() {{\n\t{}\n}}", { i(0) })),
+  -- LuaSnip syntax
+  s({
+    trig = "iferr",
+    name = "Handle error",
+    dscr = "If error does not equal nil, handle error or return",
+    docstring = "if err != nil {\n\treturn err\n}",
+  }, {
+    t({ "if err != nil {", "\t" }),
+    i(0, "return err"),
+    t({ "", "}" }),
+  }),
+  -- LSP syntax
+  parse({
+    trig = "func",
+  }, "func $1($2) $3 {\n\t$0\n}"),
+  -- "fmt" syntax
+  s({
+    trig = "fmain",
+    docstring = "func main() {\n\t\n}",
+  }, fmt("func main() {{\n\t{}\n}}", { i(0) })),
 })
 
 ls.add_snippets("svelte", {
-	s({
-		trig = "ts_script_tag",
-		name = "TypeScript <script> tag",
-		docstring = "<script lang=\"ts\">"
-	}, {
-		t({ "<script lang=\"ts\">", "\t" }),
-		i(0),
-		t({ "", "</script>" })
-	}),
-	s("onmount", {
-		t({ "import { onMount } from \"svelte\"", "" }),
-		t("onMount(("),
-		i(1),
-		t({ ") => {", "\t" }),
-		i(0),
-		t({ "", "})" })
-	}),
-	s("load_fn", {
-		t({ "import type { Load } from '@sveltejs/kit'", "", "" }),
-		t("export const load: Load = async ({ "),
-		i(1, "url, params, fetch"),
-		t({ " }) => {", "\t" }),
-		i(0),
-		t({ "", "\t" }),
-		t({ "return {", "\t\t" }),
-		t({ "props: {", "\t\t\t" }),
-		t({ "/* payload */", "\t\t" }),
-		t({ "}", "\t" }),
-		t({ "}", "" }),
-		t("}")
+  s({
+    trig = "ts_script_tag",
+    name = "TypeScript <script> tag",
+    docstring = "<script lang=\"ts\">"
+  }, {
+    t({ "<script lang=\"ts\">", "\t" }),
+    i(0),
+    t({ "", "</script>" })
+  }),
+  s("onmount", {
+    t({ "import { onMount } from \"svelte\"", "" }),
+    t("onMount(("),
+    i(1),
+    t({ ") => {", "\t" }),
+    i(0),
+    t({ "", "})" })
+  }),
+  s("load_fn", {
+    t({ "import type { Load } from '@sveltejs/kit'", "", "" }),
+    t("export const load: Load = async ({ "),
+    i(1, "url, params, fetch"),
+    t({ " }) => {", "\t" }),
+    i(0),
+    t({ "", "\t" }),
+    t({ "return {", "\t\t" }),
+    t({ "props: {", "\t\t\t" }),
+    t({ "/* payload */", "\t\t" }),
+    t({ "}", "\t" }),
+    t({ "}", "" }),
+    t("}")
 
-	})
+  })
+})
+
+ls.add_snippets("markdown", {
+  s({
+    trig = "---",
+    name = "New Hugo page",
+    dscr = "Creates a new Hugo page with front matter template.",
+    docstring = "---\ntitle: \ndescription: \nkeywords: []\ncontentType: \n---",
+  }, {
+    t({ "---", "" }),
+    t { ("title: ") },
+    i(1),
+    t({ "", "description: " }),
+    i(2),
+    t({ "", "keywords: [" }),
+    i(3),
+    t({ "]", "contentType: " }),
+    i(4),
+    t({ "", "---", "", "" }),
+    i(0)
+  })
 })
