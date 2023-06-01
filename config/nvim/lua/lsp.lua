1d1b4c422eec11a2921cb924228a7776531f7266
David Karlsson <david.karlsson@docker.com>
2023-06-01 14:26:45 +0200

    improve markdown stuff
    Signed-off-by: David Karlsson <david.karlsson@docker.com>

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -54,6 +54,7 @@ local servers = {
   pyright = {},
   tsserver = {},
   marksman = {},
+  html = {},
   jsonls = {},
   tailwindcss = {},
   lua_ls = {
@@ -82,8 +83,9 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.mdformat,
+    -- null_ls.builtins.formatting.mdformat,
     -- null_ls.builtins.diagnostics.markdownlint,
+    null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.black,
     null_ls.builtins.diagnostics.vale,
   },
