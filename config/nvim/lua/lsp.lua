bf9ad1aa387087e41be25469416ae3a012df8e38
David Karlsson <35727626+dvdksn@users.noreply.github.com>
2023-08-16 08:54:50 +0200

    dockerlsp

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -50,6 +50,8 @@ end
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
   astro = {},
+  docker_compose_language_service = {},
+  dockerls = {},
   gopls = {},
   pyright = {},
   tsserver = {},
