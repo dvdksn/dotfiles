7427566bd838b2cdb4e0da990ba519f16189ff02
David Karlsson <35727626+dvdksn@users.noreply.github.com>
2024-09-06 12:30:40 +0200

    fix null-ls setup
    Signed-off-by: David Karlsson <35727626+dvdksn@users.noreply.github.com>

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -96,13 +96,13 @@ return {
     "nvimtools/none-ls.nvim",
     config = function()
       local null_ls = require("null-ls")
-      return {
+      null_ls.setup({
         sources = {
           null_ls.builtins.diagnostics.markdownlint,
           null_ls.builtins.formatting.prettier,
           null_ls.builtins.formatting.black,
         },
-      }
+      })
     end
   },
 }
