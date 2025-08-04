5876172bc63ad14ad3de6145ebc38782c215b56d
David Karlsson <35727626+dvdksn@users.noreply.github.com>
2025-08-04 14:04:37 +0200

    pto housekeeping
    Signed-off-by: David Karlsson <35727626+dvdksn@users.noreply.github.com>

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -3,6 +3,7 @@ return {
 	'hrsh7th/nvim-cmp',
 	dependencies = {
 		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-nvim-lsp-signature-help',
 		'hrsh7th/cmp-buffer',
 		'hrsh7th/cmp-path',
 		'L3MON4D3/LuaSnip',
@@ -56,6 +57,7 @@ return {
 				{ name = 'path' },
 				{ name = 'luasnip' },
 				{ name = 'buffer' },
+				{ name = 'nvim_lsp_signature_help' },
 			},
 		}
 	end
--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -77,16 +77,6 @@ return {
       -- Enable LSP capabilities
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Enable servers
-      mason_lspconfig.setup_handlers {
-        function(server_name)
-          require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-          }
-        end,
-      }
     end
   },
   {
