bf2f1a5058fb8854e9110a5ce9b7d7c73b51c5da
David Karlsson <35727626+dvdksn@users.noreply.github.com>
2025-08-15 16:44:20 +0200

    nvim: fix lsp config
    Signed-off-by: David Karlsson <35727626+dvdksn@users.noreply.github.com>

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,4 +1,4 @@
--- Language servers config
+-- servers that need extra "settings"
 local servers = {
   gopls = {},
   html = {},
@@ -11,32 +11,24 @@ local servers = {
       diagnostics = {
         disable = { "missing-fields", "inject-fields" },
         globals = { "vim" },
-      }
+      },
     },
   },
   marksman = {},
   pyright = {},
   tailwindcss = {},
   ts_ls = {},
-  vale_ls = {
-    installVale = false,
-  },
+  vale_ls = { installVale = false },
   yamlls = {},
 }
 
--- on_attach for LSP buffers
 local on_attach = function(_, bufnr)
   local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
+    if desc then desc = 'LSP: ' .. desc end
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
-
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
@@ -44,40 +36,47 @@ local on_attach = function(_, bufnr)
   nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
-  -- `:Format` command for LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function()
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
 end
 
 return {
   {
-    "neovim/nvim-lspconfig",
+    "mason-org/mason.nvim",
+    opts = {},
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
     dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
       "hrsh7th/cmp-nvim-lsp",
     },
-    config = function(lspconfig)
-      -- Setup mason so it can manage external tooling
-      require('mason').setup()
+    opts = {
+      ensure_installed = vim.tbl_keys(servers),
+      automatic_enable = true,
+    },
+    config = function(_, opts)
+      require("mason-lspconfig").setup(opts)
 
-      local mason_lspconfig = require 'mason-lspconfig'
-
-      -- Ensure the servers above are installed
-      mason_lspconfig.setup {
-        ensure_installed = vim.tbl_keys(servers),
-      }
-
-      -- Enable LSP capabilities
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    end
+      -- global defaults
+      vim.lsp.config("*", {
+        on_attach = on_attach,
+        capabilities = capabilities,
+      })
+
+      -- per-server settings
+      for name, cfg in pairs(servers) do
+        if cfg and next(cfg) ~= nil then
+          vim.lsp.config(name, { settings = cfg })
+        end
+      end
+    end,
   },
   {
     "nvimtools/none-ls.nvim",
