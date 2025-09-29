847d73b64cbe87b6657530ae2e130ec54c82c559
David Karlsson <35727626+dvdksn@users.noreply.github.com>
2025-09-29 20:26:19 +0200

    nvim: fix missing on_attach
    Signed-off-by: David Karlsson <35727626+dvdksn@users.noreply.github.com>

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -83,6 +83,7 @@ return {
     config = function()
       local null_ls = require("null-ls")
       null_ls.setup({
+        on_attach = on_attach,
         sources = {
           null_ls.builtins.diagnostics.markdownlint,
           null_ls.builtins.formatting.prettier,
